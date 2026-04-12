cb116a9e72a5addde170fde9c76fc35b57476de2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-04-12 21:07:33 +0200

    Read attribution from GeowikiAPI if it is not passed via options

--- src/OverpassLayer.js
+++ src/OverpassLayer.js
@@ -175,9 +175,34 @@ class OverpassLayer {
     this.emit('globalTwigData', this.globalTwigData)
   }
 
+  /**
+   * set or clear attribution. if cleared, it will read/request the
+   * attribution from GeowikiAPI (either geowikiAPI.options.attribution or
+   * geowikiAPI.meta.copyright).
+   * @param {string} [attribution] A HTML string
+   * containing the attribution.
+   */
+  setAttribution (attribution = null) {
+    // TODO: sanitize HTML string
+    // TODO: in GeowikiAPI provide a getAttribution() function
+    if (attribution === null) {
+      if (this.geowikiAPI.options.attribution || this.geowikiAPI.meta) {
+        attribution = this.geowikiAPI.options.attribution ?? this.geowikiAPI.meta.copyright
+      } else {
+        this.geowikiAPI.once('load', () => this.setAttribution())
+      }
+    }
+
+    this.options.attribution = attribution
+  }
+
   check_update_map () {
     if (!this.map || !this.map._loaded) {
       return
+    }
+
+    if (!this.options.attribution) {
+      this.setAttribution()
     }
 
     const queryOptions = JSON.parse(JSON.stringify(this.options.queryOptions))
